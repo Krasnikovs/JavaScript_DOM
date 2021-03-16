7c24f5e1087ac6d884bb0e86d20ab7c163129f2f
Krasnikovs <ralis3333@gmail.com>
2021-03-16 14:12:58 +0200

    Lesson 11

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const list = document.querySelector('#book-list ul')
 
+//delete
 list.addEventListener('click', function(e){
     if(e.target.className == 'delete'){
         const li = e.target.parentElement;
         li.parentNode.removeChild(li);
     }
-})
+});
+
+//add book
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+});
